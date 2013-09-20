0b8027295fec66c7ca62f6c813c088926a96e7bc
gdi2290 <github@gdi2290.com>
2013-09-20 12:25:08 -0700

    setup tests

---
+++ test/angular-raven_test.js
@@ -0,0 +1,36 @@
+'use strict';
+
+// var angularRaven = require('../angular-raven.js');
+
+/*
+  ======== A Handy Little Nodeunit Reference ========
+  https://github.com/caolan/nodeunit
+
+  Test methods:
+    test.expect(numAssertions)
+    test.done()
+  Test assertions:
+    test.ok(value, [message])
+    test.equal(actual, expected, [message])
+    test.notEqual(actual, expected, [message])
+    test.deepEqual(actual, expected, [message])
+    test.notDeepEqual(actual, expected, [message])
+    test.strictEqual(actual, expected, [message])
+    test.notStrictEqual(actual, expected, [message])
+    test.throws(block, [error], [message])
+    test.doesNotThrow(block, [error], [message])
+    test.ifError(value)
+*/
+
+exports['angularRaven'] = {
+  setUp: function(done) {
+    // setup here
+    done();
+  },
+  'no args': function(test) {
+    test.expect(1);
+    // tests here
+    test.equal(1, 1, 'pass the test.');
+    test.done();
+  }
+};
