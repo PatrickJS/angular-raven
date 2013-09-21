f58774dd0e5ae5abcf21dbf6e357cbcbc7ae24c6
gdi2290 <github@gdi2290.com>
2013-09-21 11:34:45 -0700

    WIP raven global checking

--- angular-raven.js
+++ angular-raven.js
@@ -1,7 +1,25 @@
-(function() {
+;(function(__global, __document, __Raven) {
   "use strict";
+  if (__Raven) {
+    var scriptRaven = {
+      raven: __document.createElement('script');
+      script:
+      load: function() {
+        __document.getElementsByTagName('head').appendChild(this.raven);
+      },
+      init: function() {
+        this.script.type = 'text/javascript';
+        this.script.async = true;
+        this.script.src = '//cdnjs.cloudflare.com/ajax/libs/raven.js/1.0.8/raven.min.js';
+        this.load();
+      };
+    }
+  }
 
-  angular.module('angular-raven', ['ng'])
+  angular.module('angular-raven', [])
+    .provider('$Raven', function() {
+
+    })
     .factory('$exceptionHandler', ['$window', '$log',
       function($window, $log) {
         if ($window.Raven) {
@@ -17,5 +35,5 @@
       }
     ]);
 
-}());
+}(window, document, Raven, undefined));
 
