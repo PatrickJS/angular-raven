d4d925840dcc3dd9b947cc0630ce254881f2b5c0
PatrickJS <github@gdi2290.com>
2013-10-21 08:35:55 -0700

    remove 'ng'

--- angular-raven.js
+++ angular-raven.js
@@ -1,21 +1,20 @@
-(function() {
-  "use strict";
+(function(module) {
+"use strict";
 
-  angular.module('angular-raven', ['ng'])
-    .factory('$exceptionHandler', ['$window', '$log',
-      function($window, $log) {
-        if ($window.Raven) {
-          return function(exception, cause) {
-            $log.error.apply($log, arguments);
-            Raven.captureException(exception);
-          };
-        } else {
-          return function(exception, cause) {
-            $log.error.apply($log, arguments);
-          };
-        }
-      }
-    ]);
+module.factory('$exceptionHandler', ['$window', '$log',
+  function($window, $log) {
+    if ($window.Raven) {
+      return function(exception, cause) {
+        $log.error.apply($log, arguments);
+        Raven.captureException(exception);
+      };
+    } else {
+      return function(exception, cause) {
+        $log.error.apply($log, arguments);
+      };
+    }
+  }
+]);
 
-}());
+}(  angular.module('angular-raven', [])));
 
