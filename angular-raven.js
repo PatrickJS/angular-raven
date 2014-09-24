8eca93a75c0e797add09f034686bf5ecf0a9eef9
gdi2290 <github@gdi2290.com>
2014-09-24 12:51:56 -0700

    refactor(Raven): add setUser and clean up

--- angular-raven.js
+++ angular-raven.js
@@ -1,106 +1,122 @@
-(function(module, angular, undefined) {
-'use strict';
+!function(module, angular, undefined) {
+  'use strict';
 
-module.provider('Raven', function() {
-  var _development = null;
+  module.provider('Raven', function() {
+    var _development = null;
 
-  this.development = function(config) {
-    _development = config || _development;
-    return this;
-  };
-
-  this.$get = ['$window', '$log', function($window, $log) {
-    var service = {
-      VERSION: ($window.Raven) ? $window.Raven.VERSION : 'development',
-      TraceKit: ($window.Raven) ? $window.Raven.TraceKit : 'development',
-      captureException: function captureException(exception, cause) {
-        if (_development) {
-          $log.error('Raven: Exception ', exception, cause);
-        } else {
-          $window.Raven.captureException(exception, cause);
-        }
-      },
-      captureMessage: function captureMessage(message, data) {
-        if (_development) {
-          $log.error('Raven: Message ', message, data);
-        } else {
-          $window.Raven.captureMessage(message, data);
-        }
-      },
-      setUserContext: function setUserContext(user) {
-        if (_development) {
-          $log.info('Raven: User ', user);
-        } else {
-          $window.Raven.setUserContext(user);
-        }
-      },
-      lastException: function lastException() {
-        if (_development) {
-          $log.error('Raven: Last Exception');
-        } else {
-          $window.Raven.lastException();
-        }
-      },
-      context: function(options, func, args) {
-        var RavenService = this;
-
-        if (angular.isFunction(options)) {
-          args = func || [];
-          func = options;
-          options = undefined;
-        }
-
-        return RavenService.wrap(options, func).apply(this, args);
-      },
-      wrap: function(options, func) {
-        var RavenService = this;
-
-        if (angular.isUndefined(func) && !angular.isFunction(options)) {
-          return options;
-        }
-
-        if (angular.isFunction(options)) {
-          func = options;
-          options = undefined;
-        }
-
-        if (!angular.isFunction(func)) {
-          return func;
-        }
-
-        if (func.__raven__) {
-          return func;
-        }
-
-        function wrapped() {
-          var args = [], i = arguments.length;
-          while(i--) args[i] = RavenService.wrap(options, arguments[i]);
-          try {
-            return func.apply(this, args);
-          } catch(e) {
-            RavenService.captureException(e, options);
-          }
-        }
-
-        for (var property in func) {
-          if (func.hasOwnProperty(property)) {
-            wrapped[property] = func[property];
-          }
-        }
-        wrapped.__raven__ = true;
-        return wrapped;
-      }
-
+    this.development = function(config) {
+      _development = config || _development;
+      return this;
     };
 
-    return service;
-  }]; // end $get
-}); // end provider
+    this.$get = ['$window', '$log', function($window, $log) {
+      var service = {
+        VERSION: ($window.Raven) ? $window.Raven.VERSION : 'development',
+        TraceKit: ($window.Raven) ? $window.Raven.TraceKit : 'development',
+        captureException: function captureException(exception, cause) {
+          if (_development) {
+            $log.error('Raven: Exception ', exception, cause);
+          } else {
+            $window.Raven.captureException(exception, cause);
+          }
+        },
+        captureMessage: function captureMessage(message, data) {
+          if (_development) {
+            $log.error('Raven: Message ', message, data);
+          } else {
+            $window.Raven.captureMessage(message, data);
+          }
+        },
+        setUser: function setUser(user) {
+          if (_development) {
+            $log.info('Raven: User ', user);
+          } else {
+            if ($window.Raven.setUser) {
+              $window.Raven.setUser(user);
+            } else if ($window.Raven.setUserContext) {
+              $window.Raven.setUserContext(user);
+            }
+          }
+        },
+        setUserContext: function setUserContext(user) {
+          if (_development) {
+            $log.info('Raven: User ', user);
+          } else {
+            $window.Raven.setUserContext(user);
+            if ($window.Raven.setUser) {
+              $window.Raven.setUserContext(user);
+            } else if ($window.Raven.setUserContext) {
+              $window.Raven.setUser(user);
+            }
+          }
+        },
+        lastException: function lastException() {
+          if (_development) {
+            $log.error('Raven: Last Exception');
+          } else {
+            $window.Raven.lastException();
+          }
+        },
+        context: function context(options, func, args) {
+          var RavenService = this;
 
-module.factory('$exceptionHandler', ['Raven', function(Raven) {
-  return function(exception, cause) {
-    Raven.captureException(exception, cause);
-  };
-}]);
+          if (angular.isFunction(options)) {
+            args = func || [];
+            func = options;
+            options = undefined;
+          }
 
-}(angular.module('ngRaven', []), angular));
+          return RavenService.wrap(options, func).apply(RavenService, args);
+        },
+        wrap: function wrap(options, func) {
+          var RavenService = this;
+
+          if (angular.isUndefined(func) && !angular.isFunction(options)) {
+            return options;
+          }
+
+          if (angular.isFunction(options)) {
+            func = options;
+            options = undefined;
+          }
+
+          if (!angular.isFunction(func)) {
+            return func;
+          }
+
+          if (func.__raven__) {
+            return func;
+          }
+
+          function wrapped() {
+            var args = [], i = arguments.length;
+            while(i--) args[i] = RavenService.wrap(options, arguments[i]);
+            try {
+              return func.apply(this, args);
+            } catch(e) {
+              RavenService.captureException(e, options);
+            }
+          }
+
+          for (var property in func) {
+            if (func.hasOwnProperty(property)) {
+              wrapped[property] = func[property];
+            }
+          }
+          wrapped.__raven__ = true;
+          return wrapped;
+        }
+
+      };
+
+      return service;
+    }]; // end $get
+  }); // end provider
+
+  module.factory('$exceptionHandler', ['Raven', function(Raven) {
+    return function(exception, cause) {
+      Raven.captureException(exception, cause);
+    };
+  }]);
+
+}(angular.module('ngRaven', []), angular);
