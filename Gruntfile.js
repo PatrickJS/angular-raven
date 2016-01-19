31ea4b521de44e0a075d848419e881c24c87e000
Rafael Belliard <belliard@gmail.com>
2016-01-19 10:13:03 -0400

    feat: implement the `grunt-bump` package

--- Gruntfile.js
+++ Gruntfile.js
@@ -48,7 +48,25 @@ module.exports = function(grunt) {
           'angular-raven.min.js': ['angular-raven.js'],
         },
       },
-    }
+    },
+    bump: {
+      options: {
+        files: ['package.json', 'bower.json'],
+        updateConfigs: [],
+        commit: true,
+        commitMessage: 'Release v%VERSION%',
+        commitFiles: ['package.json', 'bower.json'],
+        createTag: true,
+        tagName: 'v%VERSION%',
+        tagMessage: 'Version %VERSION%',
+        push: false,
+        pushTo: 'origin',
+        gitDescribeOptions: '--tags --always --abbrev=1 --dirty=-d',
+        globalReplace: false,
+        prereleaseName: false,
+        regExp: false,
+      },
+    },
   });
 
   // Default task.
